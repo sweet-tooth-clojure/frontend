74df98f61d8d34ed0e2f01073edf46046cddb34e
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2018-11-11 14:59:41 -0500

    update form components

--- src/sweet_tooth/frontend/form/components.cljs
+++ src/sweet_tooth/frontend/form/components.cljs
@@ -49,18 +49,47 @@
 (defn label-for [form-id attr-path]
   (str form-id (attr-path-str attr-path)))
 
+(defn input-key
+  [{:keys [form-id partial-form-path attr-path]} & suffix]
+  (str form-id partial-form-path attr-path (str/join "" suffix)))
+
+(defn pre-wrap
+  [f1 f2]
+  (fn [& args]
+    (apply f2 args)
+    (apply f1 args)))
+
+(defn post-wrap
+  [f1 f2]
+  (fn [& args]
+    (apply f1 args)
+    (apply f2 args)))
+
 ;;~~~~~~~~~~~~~~~~~~
-;; input
+;; input opts
 ;;~~~~~~~~~~~~~~~~~~
+
+;; used in the field component below
+(def field-opts #{:tip :before-input :after-input :after-errors :label :no-label})
 
 ;; react doesn't recognize these and hates them
 (def custom-opts #{:attr-buffer :attr-path :attr-errors
-                   :no-label :options :partial-form-path
+                   :label :no-label :options
+                   :partial-form-path :input-type
                    :format-read :format-write})
 
 (defn dissoc-custom-opts
   [x]
-  (apply dissoc x custom-opts))
+  (apply dissoc x (into custom-opts field-opts)))
+
+
+(defn framework-input-opts
+  [{:keys [partial-form-path attr-path] :as opts}]
+  (merge {:attr-buffer  (subscribe [::stff/attr-buffer partial-form-path attr-path])
+          :attr-errors  (subscribe [::stff/attr-errors partial-form-path attr-path])
+          :format-read  identity
+          :format-write identity}
+         opts))
 
 (defn on-change-fn
   [{:keys [attr-path partial-form-path format-write]
@@ -71,86 +100,58 @@
   [{:keys [attr-path partial-form-path]}]
   #(dispatch-touch partial-form-path attr-path))
 
-(defn input-opts
-  [{:keys [form-id attr-path attr-buffer partial-form-path format-read format-write]
-    :or   {format-read  identity
-           format-write identity}
+(defn input-type-opts-default
+  [{:keys [form-id attr-path attr-buffer format-read input-type]
     :as   opts}]
-  (-> {:value     (format-read @attr-buffer)
-       :id        (label-for form-id attr-path)
-       :on-change (on-change-fn opts)
-       :on-blur   (on-blur-fn opts)
-       :class     (str "input " (attr-path-str attr-path))}
-      (merge opts)
-      (dissoc-custom-opts)))
+  (merge {:type      (name (or input-type :text))
+          :value     (format-read @attr-buffer)
+          :id        (label-for form-id attr-path)
+          :on-change (on-change-fn opts)
+          :on-blur   (on-blur-fn opts)
+          :class     (str "input " (attr-path-str attr-path))}
+         opts))
 
-(defn input-key
-  [{:keys [form-id partial-form-path attr-path]} & suffix]
-  (str form-id partial-form-path attr-path (str/join "" suffix)))
+(defmulti input-type-opts :input-type)
 
-(defmulti input (fn [type _] type))
+(defmethod input-type-opts :default
+  [opts]
+  (input-type-opts-default opts))
 
-(defmethod input :textarea
-  [type opts]
-  [:textarea (input-opts opts)])
+(defmethod input-type-opts :textarea
+  [opts]
+  (-> (input-type-opts-default opts)
+      (dissoc :type)))
 
-(defmethod input :select
-  [type {:keys [options attr-buffer format-read]
-         :or   {format-read  identity
-                format-write identity}
-         :as   opts}]
-  [:select (merge (input-opts opts) {:value (format-read @attr-buffer)})
-   (for [[opt-value txt option-opts] options]
-     ^{:key (input-key opts opt-value)}
-     [:option (cond-> {}
-                opt-value (assoc :value opt-value)
-                true      (merge option-opts))
-      txt])])
+(defmethod input-type-opts :select
+  [opts]
+  (-> (input-type-opts-default opts)
+      (dissoc :type)))
 
-(defmethod input :radio
-  [type {:keys [options partial-form-path attr-path attr-buffer format-read]
-         :or   {format-read  identity
-                format-write identity}
-         :as   opts}]
-  [:ul.radio
-   (doall (for [[v txt] options]
-            ^{:key (input-key opts v)}
-            [:li [:label
-                  [:input (-> opts
-                              dissoc-custom-opts
-                              (merge {:type      "radio"
-                                      :checked   (= v (format-read @attr-buffer))
-                                      :on-change #(dispatch-change partial-form-path attr-path v)}))]
-                  [:span txt]]]))])
+(defmethod input-type-opts :radio
+  [{:keys [format-read attr-buffer partial-form-path attr-path value] :as opts}]
+  (assoc (input-type-opts-default opts)
+         :checked (= value (format-read @attr-buffer))
+         :on-change #(dispatch-change partial-form-path attr-path value)))
 
-(defmethod input :checkbox
-  [type {:keys [form-id attr-buffer partial-form-path attr-path format-read]
-         :or   {format-read  identity
-                format-write identity}
-         :as   opts}]
-  (let [value (format-read @attr-buffer)
-        opts  (dissoc (input-opts opts) :value)]
-    [:input (merge opts
-                   {:type            "checkbox"
-                    :on-change       #(dispatch-change partial-form-path attr-path (not value))
-                    :default-checked (boolean value)})]))
+(defmethod input-type-opts :checkbox
+  [{:keys [attr-buffer partial-form-path attr-path format-read] :as opts}]
+  (let [value (format-read @attr-buffer)]
+    (-> (input-type-opts-default opts)
+        (assoc :default-checked (boolean value)
+               :on-change #(dispatch-change partial-form-path attr-path (not value)))
+        (dissoc :value))))
 
 (defn toggle-set-membership
   [s v]
   ((if (s v) disj conj) s v))
 
-(defmethod input :checkbox-set
-  [type {:keys [form-id attr-buffer partial-form-path attr-path options value format-read]
-         :or   {format-read  identity
-                format-write identity}
-         :as   opts}]
-  (let [checkbox-set (or (format-read @attr-buffer) #{})
-        opts         (input-opts opts)]
-    [:input (-> opts
-                dissoc-custom-opts
-                (merge {:type      "checkbox"
-                        :checked   (boolean (checkbox-set value))
-                        :on-change #(dispatch-change partial-form-path attr-path (toggle-set-membership checkbox-set value))}))]))
+(defmethod input-type-opts :checkbox-set
+  [{:keys [attr-buffer partial-form-path attr-path value format-read] :as opts}]
+  (let [checkbox-set (or (format-read @attr-buffer) #{})]
+    (assoc (input-type-opts-default opts)
+           :type      "checkbox"
+           :checked   (boolean (checkbox-set value))
+           :on-change #(dispatch-change partial-form-path attr-path (toggle-set-membership checkbox-set value)))))
 
 ;; date handling
 (defn unparse [fmt x]
@@ -166,27 +167,46 @@
             date (js/Date. (ct/year date) (dec (ct/month date)) (ct/day date))]
         (dispatch-change partial-form-path attr-path date)))))
 
-(defmethod input :date
-  [type {:keys [form-id attr-buffer partial-form-path attr-path]}]
-  [:input {:type "date"
-           :value (unparse date-fmt @attr-buffer)
-           :id (label-for form-id attr-path)
-           :on-change #(handle-date-change % partial-form-path attr-path)}])
+(defmethod input-type-opts :date
+  [{:keys [attr-buffer partial-form-path attr-path] :as opts}]
+  (assoc (input-type-opts-default opts)
+         :value (unparse date-fmt @attr-buffer)
+         :on-change #(handle-date-change % partial-form-path attr-path)))
 
-(defmethod input :number
-  [type {:keys [form-id partial-form-path attr-path] :as opts}]
-  [:input (merge (input-opts opts)
-                 {:type (name type)
-                  :on-change #(let [v (js/parseInt (u/tv  %))]
-                                (if (js/isNaN v)
-                                  (dispatch-change partial-form-path attr-path nil)
-                                  (dispatch-change partial-form-path attr-path (js/parseInt v))))})])
+(defmethod input-type-opts :number
+  [{:keys [partial-form-path attr-path] :as opts}]
+  (assoc (input-type-opts-default opts)
+         :on-change #(let [v (js/parseInt (u/tv %))
+                           v (if (js/isNaN v) nil v)]
+                       (dispatch-change partial-form-path attr-path v))))
+
+;;~~~~~~~~~~~~~~~~~~
+;; input components
+;;~~~~~~~~~~~~~~~~~~
+
+(defmulti input :input-type)
+
+(defmethod input :textarea
+  [opts]
+  [:textarea opts])
+
+(defmethod input :select
+  [{:keys [options]
+    :as   opts}]
+  [:select opts
+   (for [[opt-value txt option-opts] options]
+     ^{:key (input-key opts opt-value)}
+     [:option (cond-> {}
+                opt-value (assoc :value opt-value)
+                true      (merge option-opts))
+      txt])])
 
 (defmethod input :default
-  [type {:keys [form-id] :as opts}]
-  [:input (merge (input-opts opts) {:type (name type)})])
+  [opts]
+  [:input opts])
+
 ;;~~~~~~~~~~~~~~~~~~
-;; end input
+;; 'field' interface, wraps inputs with error messagse and labels
 ;;~~~~~~~~~~~~~~~~~~
 
 (defn error-messages
@@ -196,29 +216,13 @@
     [:ul {:class "error-messages"}
      (map (fn [x] ^{:key (str "error-" x)} [:li x]) errors)]))
 
-(defn field-row
-  "Structure the field as a table row"
-  [type {:keys [form-id attr-path attr-errors required] :as opts}]
-  [:tr {:class (when @attr-errors "error")}
-   [:td [:label {:for (label-for form-id attr-path) :class "label"}
-         (label-text attr-path)
-         (if required [:span {:class "required"} "*"])]]
-   [:td [input type opts]
-    (error-messages @attr-errors)]])
-
-(defn path->class
-  [path]
-  (->> path
-       (filter (comp not number?))
-       (filter identity)
-       (map name)
-       (str/join " ")))
-
-(defmulti field (fn [type _] type))
+(defmulti field :input-type)
 
 (defmethod field :default
-  [type {:keys [form-id tip attr-path attr-errors required label no-label
-                before-input after-input after-errors] :as opts}]
+  [{:keys [form-id attr-path attr-errors
+           tip required label no-label
+           before-input after-input after-errors]
+    :as opts}]
   [:div.field {:class (str (u/kebab (attr-path-str attr-path)) (when @attr-errors "error"))}
    (when-not no-label
      [:label {:for (label-for form-id attr-path) :class "label"}
@@ -227,21 +231,21 @@
    (when tip [:div.tip tip])
    [:div
     before-input
-    [input type (dissoc opts :tip :before-input :after-input :after-errors)]
+    [input (dissoc-custom-opts opts)]
     after-input
     (error-messages @attr-errors)
     after-errors]])
 
 (defn checkbox-field
-  [type {:keys [data form-id tip required label no-label
-                attr-path attr-errors]
-         :as opts}]
+  [{:keys [data form-id tip required label no-label
+           attr-path attr-errors]
+    :as opts}]
   [:div.field {:class (str (u/kebab (attr-path-str attr-path)) (when @attr-errors "error"))}
    [:div
     (if no-label
-      [:span [input type (dissoc opts :tip)] [:i]]
+      [:span [input (apply dissoc opts field-opts)] [:i]]
       [:label {:class "label"}
-       [input type (dissoc opts :tip)]
+       [input (dissoc-custom-opts opts)]
        [:i]
        (or label (label-text attr-path))
        (when required [:span {:class "required"} "*"])])
@@ -249,74 +253,83 @@
     (error-messages @attr-errors)]])
 
 (defmethod field :checkbox
-  [type opts]
-  (checkbox-field type opts))
+  [opts]
+  (checkbox-field opts))
 
 (defmethod field :checkbox-set
-  [type opts]
-  (checkbox-field type opts))
+  [opts]
+  (checkbox-field opts))
 
-(defn build-input-opts
-  "Merges the input option hierarchy: 
 
-  1. options provided by the framework
-  2. options that are meant to be applied to every input for this form
-  3. options for this specific input
+(defn field-component
+  "Adapts the interface to `field` so that the caller can supply either
+  a) a map of opts as the only argument or b) an `input-type`,
+  `attr-path`, and `input-opts`.
 
-  `formwide-input-opts` and `input-opts` can be functions, allowing
-  them access to the framework opts. This is so that custom input
-  handlers like `:on-blur` can have access to the same context as
-  framework handlers like `:on-change`."
-  [partial-form-path attr-path formwide-input-opts input-opts]
-  (let [framework-opts      {:attr-path         attr-path
-                             :attr-buffer       (subscribe [::stff/attr-buffer partial-form-path attr-path])
-                             :attr-errors       (subscribe [::stff/attr-errors partial-form-path attr-path])
-                             :partial-form-path partial-form-path}
-        formwide-input-opts (if (fn? formwide-input-opts)
-                              (formwide-input-opts framework-opts)
-                              formwide-input-opts)
-        input-opts          (if (fn? input-opts)
-                              (input-opts framework-opts)
-                              input-opts)]
-    (merge framework-opts formwide-input-opts input-opts)))
+  In the case of b, `input-opts` consists only of the opts specific to
+  this input (it doesn't include framework opts). Those opts are
+  passed to the `input-opts` function.
 
-(defn builder
-  "creates a function (component) that builds inputs"
-  [partial-form-path formwide-input-opts]
-  (fn [type attr-path & {:as input-opts}]
-    [field type (build-input-opts partial-form-path
-                                  attr-path
-                                  formwide-input-opts
-                                  input-opts)]))
+  This allows the user to call [input :text :user/username {:x :y}]
+  rather than something like
+
+  [input (all-input-opts :partial-form-path :text :user/username {:x :y})]"
+  [all-input-opts-fn]
+  (fn [input-type & [attr-path input-opts]]
+    [field (if (map? input-type)
+             input-type
+             (all-input-opts-fn input-type attr-path input-opts))]))
+
+;;~~~~~~~~~~~~~~~~~~
+;; interface fns
+;;~~~~~~~~~~~~~~~~~~
+
+(defn all-input-opts
+  [partial-form-path input-type attr-path & [opts]]
+  (-> {:partial-form-path partial-form-path
+       :input-type        input-type
+       :attr-path         attr-path}
+      (merge opts)
+      (framework-input-opts)
+      (input-type-opts)))
+
+(defn input-component
+  "Adapts the interface to `input` so that the caller can supply either
+  a) a map of opts as the only argument or b) an `input-type`,
+  `attr-path`, and `input-opts`.
+
+  In the case of b, `input-opts` consists only of the opts specific to
+  this input (it doesn't include framework opts). Those opts are
+  passed to the `input-opts` function.
+
+  This allows the user to call [input :text :user/username {:x :y}]
+  rather than something like
+
+  [input (all-input-opts :partial-form-path :text :user/username {:x :y})]"
+  [all-input-opts-fn]
+  (fn [input-type & [attr-path input-opts]]
+    [input (-> (if (map? input-type)
+                 input-type
+                 (all-input-opts-fn input-type attr-path input-opts))
+               (dissoc-custom-opts))]))
+
 (defn on-submit-handler
   [form-path & [submit-opts]]
   (u/prevent-default #(dispatch [::stff/submit-form form-path submit-opts])))
 
 (defn on-submit
   [form-path & [submit-opts]]
-  {:on-submit (apply on-submit-handler form-path submit-opts)})
+  {:on-submit (on-submit-handler form-path submit-opts)})
 
 (defn form
   "Returns an input builder function and subscriptions to all the form's keys"
   [partial-form-path & [opts]]
-  {:form-state    (subscribe [::stff/state partial-form-path])
-   :form-ui-state (subscribe [::stff/ui-state partial-form-path])
-   :form-errors   (subscribe [::stff/errors partial-form-path])
-   :form-buffer   (subscribe [::stff/buffer partial-form-path])
-   :form-dirty?   (subscribe [::stff/form-dirty? partial-form-path])
-   :input         (builder partial-form-path (:input opts))})
-
-(defn client-side-validation
-  "Returns options that you can pass to `:input` for `form`. These
-  options, passed to an input, enable client-side validation such that
-  validation is triggered on blur and performed on every change
-  thereafter."
-  [validator]
-  (fn [{:keys [partial-form-path attr-path attr-errors format-write]
-        :or {format-write identity}
-        :as opts}]
-    (let [validate #(dispatch-validation partial-form-path attr-path validator)]
-      {:on-change #(do (dispatch-change partial-form-path attr-path (format-write (u/tv %)))
-                       (when (some? @attr-errors)
-                         (validate)))
-       :on-blur validate})))
+  (let [input-opts-fn (partial all-input-opts partial-form-path)]
+    {:form-state    (subscribe [::stff/state partial-form-path])
+     :form-ui-state (subscribe [::stff/ui-state partial-form-path])
+     :form-errors   (subscribe [::stff/errors partial-form-path])
+     :form-buffer   (subscribe [::stff/buffer partial-form-path])
+     :form-dirty?   (subscribe [::stff/form-dirty? partial-form-path])
+     :input-opts    input-opts-fn
+     :input         (input-component input-opts-fn)
+     :field         (field-component input-opts-fn)}))
