d63f9dd5c78b434bfb8e20676b388bd820bb1247
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2019-01-04 16:34:57 -0500

    moving toward handlers for updating token and dispatching route

--- src/sweet_tooth/frontend/nav/handler.cljs
+++ src/sweet_tooth/frontend/nav/handler.cljs
@@ -107,7 +107,7 @@
                        (= (str port) (str current-port)))
                    (path-exists? path))
           (when (not= current-relative-href relative-href) ;; do not add duplicate html5 history state
-            (. history (setToken relative-href title)))
+            (rf/dispatch [::update-token relative-href :set title]))
           (.preventDefault e)
           (when reload-same-path?
             (events/dispatchEvent history (Event. path true))))))))
@@ -123,13 +123,14 @@
   path-exists?: a fn of one argument, a path. Return truthy if this path is handled by the SPA"
   [{:keys [match-route reload-same-path?] :as config}]  
   (let [history      (new-history)
-        nav-handler  (comp strf/dispatch-route match-route)
+        nav-handler  (fn [path] (rf/dispatch [::dispatch-route path]))
         path-exists? (comp :route-name match-route)]
-    {:nav-handler nav-handler
+    {:nav-handler  nav-handler
      :path-exists? path-exists?
-     :history history
-     :document-click-handler-listener-key (dispatch-on-navigate history nav-handler)
-     :navigate-listener-key (prevent-reload-on-known-path history path-exists? reload-same-path?)}))
+     :match-route  match-route
+     :history      history
+     :listeners    {:document-click (prevent-reload-on-known-path history path-exists? reload-same-path?)
+                    :navigate       (dispatch-on-navigate history nav-handler)}}))
 
 (defmethod ig/init-key ::handler
   [_ config]
@@ -142,7 +143,7 @@
   the call to `configure-navigation!`."
   [handler]
   (.dispose (:history handler))
-  (doseq [key (vals (select-keys handler [:document-click-handler-listener-key :navigate-listener-key]))]
+  (doseq [key (vals (:listeners handler))]
     (events/unlistenByKey key)))
 
 (defmethod ig/halt-key! ::handler
@@ -158,50 +159,82 @@
 (sth/rr rf/reg-event-fx ::navigate
   []
   (fn [cofx [route query]]
-    (let [history (get-in cofx [:db :sweet-tooth/system ::handler :history])]
-      )))
+    (let [{:keys [nav-handler history]} (get-in cofx [:db :sweet-tooth/system ::handler])]
+      (if nav-handler
+        (let [token (.getToken history)
+              old-route (first (str/split token "?"))
+              query-string (map->params (reduce-kv (fn [valid k v]
+                                                     (if v
+                                                       (assoc valid k v)
+                                                       valid)) {} query))
+              with-params (if (empty? query-string)
+                            route
+                            (str route "?" query-string))]
+          (if (= old-route route)
+            {:dispatch [::update-token with-params :replace]}
+            {:dispatch [::update-token with-params :set]}))
+        (js/console.error "can't navigate until handler is initialized")))))
+
+;; interceptor takes URL
+;; interceptor converts URL to route
+;; interceptor checks that change is allowable
+;; interceptor places results as coeffect
+(def route
+  {:id     ::route
+   :before (fn [context]
+             (let [match-route (get-in context [:coeffects :db :sweet-tooth/system ::handler :match-route])
+                   path        (get-in context [:coeffects :event 1])]
+               (assoc-in context [:coeffects ::route] (match-route path))))
+   :after  identity})
+
+;; ------
+;; dispatch route
+;; ------
+
+(sth/rr rf/reg-event-fx ::dispatch-route
+  [route]
+  (fn [cofx _]
+    {::dispatch-route (::route cofx)}))
+
+(sth/rr rf/reg-fx ::dispatch-route
+  (fn [route]
+    (strf/dispatch-route route)))
+
+;; ------
+;; dispatch current
+;; ------
 
 (defn dispatch-current-effect
   [cofx]
-  {::dispatch-current (get-in cofx [:db :sweet-tooth/system ::handler :nav-handler])})
+  {::dispatch-current (get-in cofx [:db :sweet-tooth/system ::handler :match-route])})
 
 (sth/rr rf/reg-event-fx ::dispatch-current
   []
   (fn [cofx _] (dispatch-current-effect cofx)))
 
 (sth/rr rf/reg-fx ::dispatch-current
-  (fn [nav-handler]
-    (let [path (-> js/window .-location .-pathname)
+  (fn [match-route]
+    (let [path  (-> js/window .-location .-pathname)
           query (-> js/window .-location .-search)
-          hash (-> js/window .-location .-hash)]
-      (if nav-handler
-        (nav-handler (str path query hash))
-        (js/console.error "can't dispatch-current until configure-navigation! called")))))
+          hash  (-> js/window .-location .-hash)]
+      (strf/dispatch-route (match-route (str path query hash))))))
 
-#_(defn navigate!
-    "add a browser history entry. updates window/location"
-    ([route] (navigate! route {}))
-    ([route query]
-     (if nav-handler
-       (let [token (.getToken history)
-             old-route (first (str/split token "?"))
-             query-string (map->params (reduce-kv (fn [valid k v]
-                                                    (if v
-                                                      (assoc valid k v)
-                                                      valid)) {} query))
-             with-params (if (empty? query-string)
-                           route
-                           (str route "?" query-string))]
-         (if (= old-route route)
-           (. history (replaceToken with-params))
-           (. history (setToken with-params))))
-       (js/console.error "can't navigate! until configure-navigation! called"))))
+;; ------
+;; update token
+;; ------
 
-#_(defn dispatch-current! []
-    "Dispatch current URI path."
-    (let [path (-> js/window .-location .-pathname)
-          query (-> js/window .-location .-search)
-          hash (-> js/window .-location .-hash)]
-      (if nav-handler
-        (nav-handler (str path query hash))
-        (js/console.error "can't dispatch-current until configure-navigation! called"))))
+;; TODO do I need to ensure that dispatch-route finished before update-token?
+(sth/rr rf/reg-event-fx ::update-token
+  [route]
+  (fn [cofx [_ relative-href op title]]
+    {::dispatch-route (::route cofx)
+     ::update-token   {:history       (get-in cofx [:db :sweet-tooth/system ::handler :history])
+                       :relative-href relative-href
+                       :title         title
+                       :op            op}}))
+
+(sth/rr rf/reg-fx ::update-token
+  (fn [{:keys [op history relative-href title]}]
+    (if (= op :replace)
+      (. history (replaceToken relative-href title))
+      (. history (setToken relative-href title)))))
